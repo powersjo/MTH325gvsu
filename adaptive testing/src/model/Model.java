ecffcb8d0dd143d5b046ba2ccdf96f840764402b
powersjo <Jon Powers Laptop@Anonymous.UNKNOWN>
2014-03-26 11:28:44 -0400

    testing

--- adaptive testing/src/model/Model.java
+++ adaptive testing/src/model/Model.java
@@ -1,17 +1,7 @@
-/**
- * 
- */
 package model;
 
-/**
- * @author Jon Powers Laptop
- *
- */
 public class Model {
 
-	/**
-	 * 
-	 */
 	public Model() {
 		// TODO Auto-generated constructor stub
 	}
