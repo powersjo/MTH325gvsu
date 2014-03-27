3f128c2d80139876c4c2b7588b8b92f5b91b656e
powersjo <Jon Powers Laptop@Anonymous.UNKNOWN>
2014-03-27 18:48:38 -0400

    test

--- adaptive testing/src/model/Model.java
+++ adaptive testing/src/model/Model.java
@@ -5,8 +5,18 @@ public class Model {
 	public Model() {
 		// TODO Auto-generated constructor stub
 	}
-	public void recordResult(int num) {
+	/**
+	 * record the result of the answer in a text file.
+	 * @param num
+	 */
+	public void recordResult(int num, boolean answer) {
 		
 	}
-
+	private boolean isCorrect(int subject, int question) {
+		return false;
+	}
+	public int[] nextQuestion(int subject, int question) {
+		int temp[] = new int[2];
+		return temp;
+	}
 }
--- adaptive testing/src/view/View.java
+++ adaptive testing/src/view/View.java
@@ -3,6 +3,8 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -36,10 +38,38 @@ public class View extends JFrame{
 		
 	}
 	/**
+	 * Changes the amount of buttons on the bottom JPanel.
 	 * @param num the number of buttons applicable to a multiple choice question.
 	 */
 	private void adjustButtons(int num) {
 		
 	}
-
+	/**
+	 * 
+	 * This method calls the model to get the exact question needed.
+	 * @param m
+	 * @return int[] a anrray of two values for the question subject and the question number.
+	 */
+	public int[] getQuestion (Model m) {
+		int temp[] = new int[2];
+		return temp;
+	}
+	/**
+	 * Pass local button 1, 2, 3 etc to change local button picture.
+	 * @param b button to be changed.
+	 * @param subject the suject in a 2d array of the possible subject of math.
+	 * @param num the number question within that subject in the 2d array.
+	 */
+	private void setButtonPic(JButton b, int subject, int num) {
+		
+	}
+	/**
+	 * This class with either get the next question or end the program if the test is done.
+	 * 
+	 */
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+		
+		}
+	}
 }
