0a9eeebab1582def0e74f7634ae911f725cbfd5f
powersjo <Jon Powers Laptop@Anonymous.UNKNOWN>
2014-04-12 00:39:21 -0400

    tons of updates

--- adaptive testing/src/model/Model.java
+++ adaptive testing/src/model/Model.java
@@ -5,13 +5,17 @@ import java.util.Random;
 public class Model {
 
 	private String[] subjects;
+	private int[] used;
 	private int[] number;
 	private int[] current;
 	private String[][] key;
+	private int tempRandom;
 	public Model() {
 		subjects = new String[8];
 		number = new int[10];
+		used = new int[10];
 		key = new String[8][10];
+		tempRandom = 0;
 		/*
 		 * Create the Key
 		 */
@@ -24,75 +28,75 @@ public class Model {
 		key[0][6] = "c";
 		key[0][7] = "b";
 		key[0][8] = "b";
-		key[0][9] = "d";//////////////////
-		key[1][0] = "";
-		key[1][1] = "";
-		key[1][2] = "";
-		key[1][3] = "";
-		key[1][4] = "";
-		key[1][5] = "";
-		key[1][6] = "";
-		key[1][7] = "";
-		key[1][8] = "";
-		key[1][9] = "";
-		key[2][0] = "";
-		key[2][1] = "";
-		key[2][2] = "";
-		key[2][3] = "";
-		key[2][4] = "";
-		key[2][5] = "";
-		key[2][6] = "";
-		key[2][7] = "";
-		key[2][8] = "";
-		key[2][9] = "";
-		key[3][0] = "";
-		key[3][1] = "";
-		key[3][2] = "";
-		key[3][3] = "";
-		key[3][4] = "";
-		key[3][5] = "";
-		key[3][6] = "";
-		key[3][7] = "";
-		key[3][8] = "";
-		key[3][9] = "";
-		key[4][0] = "";
-		key[4][1] = "";
-		key[4][2] = "";
-		key[4][3] = "";
-		key[4][4] = "";
-		key[4][5] = "";
-		key[4][6] = "";
-		key[4][7] = "";
-		key[4][8] = "";
+		key[0][9] = "d";
+		key[1][0] = "d";
+		key[1][1] = "a";
+		key[1][2] = "c";
+		key[1][3] = "e";
+		key[1][4] = "b";
+		key[1][5] = "d";
+		key[1][6] = "b";
+		key[1][7] = "a";
+		key[1][8] = "d";
+		key[1][9] = "a";
+		key[2][0] = "c";
+		key[2][1] = "b";
+		key[2][2] = "e";// need to evaluate this question and answer.
+		key[2][3] = "e";
+		key[2][4] = "b";
+		key[2][5] = "c";
+		key[2][6] = "b"; // check this one with the picture.
+		key[2][7] = "e";
+		key[2][8] = "e";
+		key[2][9] = "e";// not in picture hopefully this is the answer.
+		key[3][0] = "e";
+		key[3][1] = "d";
+		key[3][2] = "b";
+		key[3][3] = "a";
+		key[3][4] = "d";
+		key[3][5] = "a";
+		key[3][6] = "a";
+		key[3][7] = "d";
+		key[3][8] = "d";
+		key[3][9] = "c";
+		key[4][0] = "d";
+		key[4][1] = "c";
+		key[4][2] = "d";
+		key[4][3] = "c";
+		key[4][4] = "d";
+		key[4][5] = "c";
+		key[4][6] = "b";
+		key[4][7] = "c";//trig is not done yet
+		key[4][8] = "b";
 		key[4][9] = "";
-		key[5][0] = "";
-		key[5][1] = "";
-		key[5][2] = "";
-		key[5][3] = "";
-		key[5][4] = "";
-		key[5][5] = "";
-		key[5][6] = "";
-		key[5][7] = "";
+		key[5][0] = "a";
+		key[5][1] = "a";
+		key[5][2] = "b";
+		key[5][3] = "a";
+		key[5][4] = "d";
+		key[5][5] = "d";
+		key[5][6] = "d";
+		key[5][7] = "e";//still need to finish
 		key[5][8] = "";
 		key[5][9] = "";
-		key[6][0] = "";
-		key[6][1] = "";
-		key[6][2] = "";
-		key[6][3] = "";
-		key[6][4] = "";
-		key[6][5] = "";
-		key[6][6] = "";
-		key[6][7] = "";
-		key[6][8] = "";
-		key[6][9] = "";
-		key[7][0] = "";
-		key[7][1] = "";
-		key[7][2] = "";
-		key[7][3] = "";
-		key[7][4] = "";
-		key[7][5] = "";
-		key[7][6] = "";
-		key[7][7] = "";
+		key[6][0] = "b";
+		key[6][1] = "c";
+		key[6][2] = "c";
+		key[6][3] = "c";
+		key[6][4] = "d";
+		key[6][5] = "c";
+		key[6][6] = "d";
+		key[6][7] = "c";
+		key[6][8] = "a";
+		key[6][9] = "b";
+		key[7][0] = "d";
+		key[7][1] = "a";
+		key[7][2] = "a";
+		key[7][3] = "d";
+		key[7][4] = "b";
+		key[7][5] = "a";
+		key[7][6] = "a";
+		key[7][7] = "e";//still need to get the pictures and answers
 		key[7][8] = "";
 		key[7][9] = "";
 		
@@ -124,13 +128,27 @@ public class Model {
 	public void recordResult(String ans) {
 		
 	}
-	private boolean isCorrect(int subject, int question) {
+	private boolean isCorrect(int subject, String answer) {
+		if(key[subject][current[1]].equals(answer)) {
+			return true;
+		}
 		return false;
 	}
+	private void newRandom() {
+		Random randomGen = new Random();
+		tempRandom = randomGen.nextInt(8) + 2;
+	}
 	public int nextQuestion() {
-		int temp[] = new int[2];
-		//temp[0] = subject;
-		//temp[1] = question;
+		newRandom();
+		used[current[1]] = tempRandom;
+		for (int i = 0; i < 10; i++) {
+			if (used[i] == current[1]) {
+				newRandom();
+				i=0;
+			}
+		}
+		System.out.println(tempRandom);
+		current[1] = tempRandom;
 		return current[1];
 	}
 	public String getSubject() {
